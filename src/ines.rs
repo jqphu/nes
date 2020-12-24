b4207303bd56378918bda8b1f10144f86e3df1c8
Justin Phu <justinqphu@gmail.com>
2020-12-24 15:30:34 -0800

    [iNes] Parse the basic nes header and prg rom

---
+++ src/ines.rs
@@ -0,0 +1,94 @@
+use anyhow::{anyhow, Result};
+use log::debug;
+use std::fs::File;
+use std::io::Read;
+use std::mem;
+
+/// iNes Structure.
+pub struct NesFile {
+    /// Filename
+    filename: String,
+    // Header for the given file.
+    // header: Header,
+
+    // PrgRom buffer.
+    prg_rom: Vec<u8>,
+}
+
+/// Header of a iNes ROM
+struct Header {
+    /// PRG Rom Size in multples of 16kB.
+    ///
+    /// This is used by the CPU.
+    prg_rom_multiple_size: u8,
+    // TODO: More flags :)
+}
+
+impl Header {
+    /// Number of bytes in the header.
+    const HEADER_SIZE_BYTES: usize = 16;
+
+    /// 16 KiB is the multiple.
+    const PRG_ROM_MULTIPLE: usize = 16384;
+
+    /// Construct a header struct from the raw 16 header bytes.
+    fn new(header: [u8; Self::HEADER_SIZE_BYTES]) -> Result<Self> {
+        if header[0] != 'N' as u8
+            || header[1] != 'E' as u8
+            || header[2] != 'S' as u8
+            || header[3] != 0x1A
+        {
+            return Err(anyhow!("Invalid file magic {:?}.", header));
+        }
+
+        let result = Header {
+            prg_rom_multiple_size: header[4],
+        };
+
+        for &byte in &header[6..] {
+            if byte != 0 {
+                return Err(anyhow!("Unsupported nes file format."));
+            }
+        }
+
+        Ok(result)
+    }
+
+    /// Return the prg rom size in bytes.
+    fn get_prg_rom_size(&self) -> usize {
+        self.prg_rom_multiple_size as usize * Self::PRG_ROM_MULTIPLE
+    }
+}
+
+impl NesFile {
+    pub fn new(filename: String) -> Result<Self> {
+        debug!("Parsing filename {}", filename);
+
+        let mut f = File::open(&filename)?;
+
+        let header = {
+            // Initialized immediately after.
+            let mut header_raw: [u8; Header::HEADER_SIZE_BYTES] =
+                unsafe { std::mem::MaybeUninit::uninit().assume_init() };
+            f.read(&mut header_raw)?;
+
+            debug!("Received header: {:x?}", &header_raw);
+
+            Header::new(header_raw)?
+        };
+
+        let prg_rom = {
+            let mut buffer = vec![0; header.get_prg_rom_size()];
+
+            debug!("Rom size is: {}", &header.get_prg_rom_size());
+
+            f.take(header.get_prg_rom_size() as u64).read(&mut buffer)?;
+
+            debug!("Received prg rom: {:x?}", &buffer);
+
+            buffer
+        };
+
+        Ok(NesFile { filename, prg_rom })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Clap;
+use env_logger;
+use log::info;
+
+mod ines;
+
+/// Basic emulator for the NES.
+#[derive(Clap)]
+#[clap(version = "0.0.1", author = "Justin Phu. <justinqphu@gmail.com>")]
+struct Opts {
+    /// Nes rom to test.
+    rom: String,
+}
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    let opts: Opts = Opts::parse();
+
+    info!("Loading ROM \"{}\"", &opts.rom);
+
+    let _result = ines::NesFile::new(opts.rom)?;
+
+    Ok(())
 }
